59d4736490fd8150d8d88dc122d89b4716da9b55
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-10-24 21:12:40 +0530

    Add files via upload

---
+++ a7.py
@@ -0,0 +1,15 @@
+age=13
+weight=48.6
+name="Shijit"
+info=True
+print("Type of age is",type(age))
+print("Type of weight is",type(weight))
+print("Type of name is",type(name))
+print("Type of info is",type(info))
+print("After typecasting....")
+age=str(age)
+print(age)
+print("Type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Type of weight is",type(weight))
