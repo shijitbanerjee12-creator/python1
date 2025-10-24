fb5d1cc7141cae56227bf41635ce4f916fcae8d0
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-10-24 21:39:26 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,8 @@
+a="Congratulations"
+b="!!"
+print(a.lower())
+print("To conver the given text from lower case to upper case-")
+print(a.upper())
+print("Joining string 1 and string 2",a+b)
+revtext=a[::-1]
+print("Revesred text is",revtext)
