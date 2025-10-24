a8a4aead8b21f24b0e2b731aa20e0c9fd4a529d5
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-10-24 17:57:20 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+anant=10
+dev=29
+aashvik=22
+neerav=18
+kiyansh=26
+print("Anant's birthday is on",anant)
+print("Dev's birthday is on",dev)
+print("Aashvik's birtday is on",aashvik)
+print("Neerav's birtday is on",neerav)
+print("Kiyansh's birthday is on",kiyansh)
