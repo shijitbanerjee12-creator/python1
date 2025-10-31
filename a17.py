ac60abb5d04f1cd789d699d768d0c12dc1d8810b
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-10-31 21:05:50 +0530

    Add files via upload

---
+++ a17.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number"))
+if a<15:
+    print("Less than 15")
+    print("a is in if block")
+else:
+    print("Greater than 15")
+    print("a is in else block")
+print("a is neither in else nor in if block")
