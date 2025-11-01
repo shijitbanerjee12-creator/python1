6abb707d2db845d3a882e770ef77c216d8c0e58f
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-01 19:22:35 +0530

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,9 @@
+t=int(input("Enter today's temperature"))
+if t>40:
+    print("The day is perfect for wearing light clothes")
+elif t>20:
+    print("There is a need to wear slightly warm clothes")
+elif t>0:
+    print("The day is not at all suitable for wearing light clothes")
+else:
+    print("The day is not at all suitable for wearing light clothes")
