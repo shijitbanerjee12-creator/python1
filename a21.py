d587989548b0cb52670987dd3b6f03696aaa2470
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-02 10:54:07 +0530

    Add files via upload

---
+++ a21.py
@@ -0,0 +1,14 @@
+a=12
+b=20
+c=23
+print(a!=b)
+print(b!=c)
+d=4
+e=5
+if(d==2)!=(e==5):
+    print("True")
+f=int(input("Enter a number"))
+if f%2!=0:
+    print("Not Even")
+else:
+    print("Even")
