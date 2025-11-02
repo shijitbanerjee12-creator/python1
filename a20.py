cb4a253e29055048532f6f2930cb6e073c72f46b
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-02 10:43:41 +0530

    Add files via upload

---
+++ a20.py
@@ -0,0 +1,13 @@
+a=20
+b=12
+c=0
+if a and b and c:
+    print("all of them have a value")
+else:
+    print("atleast one does not have a value")
+d=10
+e=-10
+if d>0 or e>0:
+    print("atleast one of them is greater than 0")
+else:
+    print("none of them is 0")
