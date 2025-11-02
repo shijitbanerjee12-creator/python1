c9632bb5ccbad3886329b1045b52896040e701cf
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-02 12:36:37 +0530

    Add files via upload

---
+++ project5.py
@@ -0,0 +1,8 @@
+inp = input("Enter anything : ")
+if inp >= "a" and inp <= "z" or inp >= "A" and inp <= "Z":
+   print("input is an alphabet")
+elif inp>="0":
+   print("input is a number")
+else:
+   print("input is aspecial character")
+  
