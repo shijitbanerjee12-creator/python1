2c1f645ba804b27d778e9383d0d17aa0c72a5a6e
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-14 21:18:33 +0530

    Add files via upload

---
+++ 25.py
@@ -0,0 +1,10 @@
+x=float(input("Enter a number"))
+if(type(x) is float):
+    print("The number is float")
+else:
+    print("The number is not float")
+y=float(input("Enter second number"))
+if(x is y):
+    print("Same Identity")
+else:
+    print("Different Identities")
