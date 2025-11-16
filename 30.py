0f8df53c68639be96265d00646d927ac8ed2a0e3
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-16 10:54:16 +0530

    Add files via upload

---
+++ 30.py
@@ -0,0 +1,10 @@
+mean1=38
+wrongnum=36
+corrnum=56
+totnum=40
+sum=mean1*totnum
+print("Sum is",sum)
+num2=sum-((wrongnum)-(corrnum))
+print("Number 2 is",num2)
+mean2=num2/totnum
+print("Mean 2 is:",mean2)
