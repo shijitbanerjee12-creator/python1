895f873346713c8a93ab1c18cf2b06809602183c
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-11-16 10:43:18 +0530

    Add files via upload

---
+++ 28.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a value"))
+b=int(input("Enter a value"))
+c=int(input("Enter a value"))
+d=int(input("Enter a value"))
+v=(a+b)*c/d**a%c/b
+print("The value  of (a+b)*c//d**a%c/b is",v)
+n=input("Enter your name")
+age=float(input("Enter your age"))
+if n=="Alex" or n=="Jhon" and age<10:
+    print("Yor welcome")
+else:
+    print("Hello world")
