b2487856d183a2d64f91e556c5047119043f7be4
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-12-07 11:15:15 +0530

    Add files via upload

--- project5.py
+++ project5.py
@@ -1,8 +1,25 @@
-inp = input("Enter anything : ")
-if inp >= "a" and inp <= "z" or inp >= "A" and inp <= "Z":
-   print("input is an alphabet")
-elif inp>="0":
-   print("input is a number")
-else:
-   print("input is aspecial character")
-  
+def print_mirrored_right_triangle(rows):
+    """
+    Prints a mirrored right-angled triangle pattern.
+
+    Args:
+        rows (int): The number of rows in the triangle.
+    """
+    for i in range(1, rows + 1):
+        # Print spaces
+        for j in range(rows - i):
+            print(" ", end="")
+        # Print stars
+        for k in range(i):
+            print("*", end="")
+        print()  # Move to the next line
+
+if __name__ == "__main__":
+    try:
+        num_rows = int(input("Enter the number of rows for the mirrored right-angled triangle: "))
+        if num_rows <= 0:
+            print("Please enter a positive integer for the number of rows.")
+        else:
+            print_mirrored_right_triangle(num_rows)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
