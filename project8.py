7989d48bed3bc37589dc8dae9be72eee5389e1d5
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2025-12-26 20:02:32 +0530

    Add files via upload

---
+++ project8.py
@@ -0,0 +1,9 @@
+user_input = input("Do you want to shut down? (yes/no): ")
+
+if user_input == 'yes':
+    print("shutting down")
+elif user_input == 'no':
+    print("abort shutdown")
+else:
+    print("sorry")
+
