e3681cb9c48e0b308a44f606052ba92ea82f3f55
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-01-11 13:25:16 +0530

    Add files via upload

--- b36.py
+++ b36.py
@@ -1,7 +1,5 @@
 import math
-print(math.sin(45))
-print(math.cos(45))
-print(math.tan(45))
-print(math.ceil(45.490))
-print(math.floor(45.890))
-print(math.factorial(5))
+n=int(input("Enter a number : "))
+print("Sin of the number is : ",math.sin(n))
+print("Cos of the number is : ",math.cos(n))
+print("Tan of the number is : ",math.tan(45))
