03350fe436aa6c74dd15abad80c675deed8ec85a
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-01-21 18:27:08 +0530

    Add files via upload

---
+++ c7.py
@@ -0,0 +1,13 @@
+tuplex=("tuple", False, 3.2, 1)
+print(tuplex)
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+tuplex=tuplex+(9,)
+print(tuplex)
+tuple1=(50,60,70,10)
+print(tuple1.count(50))
+tuplex=(1,0,2,9,3,8,3,8,4,7,5,6)
+_slice=tuplex[3:5]
+print(_slice)
+_slice=tuplex[:6]
+print(_slice)
