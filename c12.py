9741d1a826a51092238b67da45803a198134dc0d
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-01-23 21:26:24 +0530

    Add files via upload

---
+++ c12.py
@@ -0,0 +1,7 @@
+country_code = {'India' : '0091',
+                    'Australia': '5626',
+                    'Zimbawe' : '2734'}
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+print("Country code for Zimbawe -")
+print(country_code.get('Zimbawe', 'Not Found'))
