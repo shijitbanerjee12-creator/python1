d826518b4acaa80d4cfc20e1f68e0662663a5b72
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-01-23 21:12:29 +0530

    Add files via upload

---
+++ c10.py
@@ -0,0 +1,21 @@
+student_data={'id1':
+    {'name' : ['Sara'],
+     'class' : ['V'],
+     'subject_integration': ['english, maths, science']},
+     'id2':
+    {'name' : ['David'],
+     'class' : ['V'],
+     'subject_integration': ['english, maths, science']},
+     'id3':
+    {'name' : ['Sara'],
+     'class' : ['V'],
+     'subject_integration': ['english, maths, science']},
+     'id4':
+    {'name' : ['Sara'],
+     'class' : ['V'],
+     'subject_integration': ['english, maths, science']},}
+result={}
+for key, value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+print(result)
