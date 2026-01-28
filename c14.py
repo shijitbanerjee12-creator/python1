a41983536dda6e85a436043f6feb2e216d4212f1
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-01-28 18:46:56 +0530

    Add files via upload

---
+++ c14.py
@@ -0,0 +1,8 @@
+setx = {"Green", "blue"}
+sety = {"Blue", "yellow"}
+print("Origional set elements:")
+print(setx)
+print(sety)
+print("Intersection of two said sets")
+setz=setx.intersection(sety)
+print(setz)
