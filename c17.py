e71f462832483b2e627ba2a1f4bd3e6802ba9aba
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-01-30 21:23:56 +0530

    Add files via upload

---
+++ c17.py
@@ -0,0 +1,13 @@
+s1 = {2 ,3 ,1}
+s2 = {': b', ': a', ': c'}
+s3 = list(zip(s1, s2))
+print(s3)
+list1 = [10,20,30,40]
+list2 = [100,200,300,400]
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+stock  = ['reliance', 'infosys', 'tca']
+prices = [2175, 1127, 2750]
+my_dict = {stock : prices for stock,
+           prices in zip(stock, prices)}
+print("\n{}".format(my_dict))
