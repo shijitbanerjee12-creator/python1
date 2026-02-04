15a84ed1c236b9b8cca4bfb1e158892ef5f429cf
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-02-04 17:59:51 +0530

    Add files via upload

--- project5.py
+++ project5.py
@@ -1,25 +1,3 @@
-def print_mirrored_right_triangle(rows):
-    """
-    Prints a mirrored right-angled triangle pattern.
-
-    Args:
-        rows (int): The number of rows in the triangle.
-    """
-    for i in range(1, rows + 1):
-        # Print spaces
-        for j in range(rows - i):
-            print(" ", end="")
-        # Print stars
-        for k in range(i):
-            print("*", end="")
-        print()  # Move to the next line
-
-if __name__ == "__main__":
-    try:
-        num_rows = int(input("Enter the number of rows for the mirrored right-angled triangle: "))
-        if num_rows <= 0:
-            print("Please enter a positive integer for the number of rows.")
-        else:
-            print_mirrored_right_triangle(num_rows)
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+a = [2,3,4,5]
+res = [val ** 2 for val in a]
+print(res)
