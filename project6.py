525a596537519bc9abd8d48201482dd2dc6abd0c
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-02-04 18:59:32 +0530

    Add files via upload

--- project6.py
+++ project6.py
@@ -1,6 +1,36 @@
-import turtle
-t = turtle.Turtle()
-for i in range(4):
-    t.forward(100)  
-    t.left(90)     
-turtle.done()
+import random
+import string
+
+def generate_random_password(length=12):
+    """
+    Generates a random password of a specified length, consisting of 
+    lowercase letters, uppercase letters, and numbers.
+    
+    Args:
+        length (int): The desired length of the password. Defaults to 12.
+
+    Returns:
+        str: The randomly generated password.
+    """
+   
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    
+    password_list = [
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits)
+    ]
+    
+    for _ in range(length - 3):
+        password_list.append(random.choice(characters))
+        
+   
+    random.shuffle(password_list)
+   
+    password = "".join(password_list)
+    
+    return password
+new_password = generate_random_password(16)
+print(f"Generated Password: {new_password}")
+default_password = generate_random_password()
+print(f"Generated Default Password: {default_password}")
