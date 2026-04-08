49212ba46bc40ba0b353f6948bc5a6792c69c33e
shijitbanerjee12-creator <shijit.banerjee12@gmail.com>
2026-04-08 17:51:37 +0530

    Add files via upload

--- project1.py
+++ project1.py
@@ -1,3 +1,16 @@
-import calendar
-for month in calendar.month_name[1:]:
-    print(month)
+import tkinter as tk
+from tkinter import messagebox
+def calculate():
+    try:
+        product = float(e1.get()) * float(e2.get())
+        lbl_res.config(text=f"Result: {product}")
+    except ValueError:
+        messagebox.showerror("Error", "Invalid Input")
+root = tk.Tk()
+root.title("Calculator")
+e1, e2 = tk.Entry(root), tk.Entry(root)
+e1.pack(); e2.pack()
+tk.Button(root, text="Multiply", command=calculate).pack()
+lbl_res = tk.Label(root, text="Result: ")
+lbl_res.pack()
+root.mainloop()
